eb923506f7bb6eb978b21b509465f5719fa829c5
kavita <139803004+kavitaLavvanshi@users.noreply.github.com>
2024-06-05 12:45:20 +0530

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,52 @@
+package com.spring;
+
+public class Address {
+
+	private int rno;
+	private String city;
+	private long pincode;
+	
+	
+
+	public int getRno() {
+		return rno;
+	}
+
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public long getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(long pincode) {
+		this.pincode = pincode;
+	}
+
+	public Address(int rno, String city, long pincode) {
+		super();
+		this.rno = rno;
+		this.city = city;
+		this.pincode = pincode;
+	}
+
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String toString() {
+		return "Address [rno=" + rno + ", city=" + city + ", pincode=" + pincode + "]";
+	}
+
+}
---
+++ App.java
@@ -0,0 +1,15 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+       ApplicationContext context = new ClassPathXmlApplicationContext("spconfig.xml"); 
+       Employee emp = context.getBean("emp1",Employee.class);
+       System.out.println(emp);
+    }
+}
